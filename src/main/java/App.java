d694de6cc09b12c5ab140b41a2fd1a51b1f823c7
AlexMaurin1981 <155809065+AlexMaurin1981@users.noreply.github.com>
2024-03-18 20:48:24 +0300

    start

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.concurrent.Callable;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean.equals(bean2));
+
+        Cat cat = applicationContext.getBean("cat",Cat.class);
+        Cat cat2 =  applicationContext.getBean("cat",Cat.class);
+        System.out.println(cat.equals(cat2));
+
+
     }
 }
